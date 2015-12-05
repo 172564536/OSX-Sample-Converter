8155565f49dcb57b937a618d76d3baed9a6b901a
Carl Taylor <carltaylor43uk@gmail.com>
2015-12-05 11:18:36 +0000

    Initial Commit

---
+++ mpc-audio/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  mpc-audio
+//
+//  Created by Carl  on 05/12/2015.
+//  Copyright © 2015 Carl Taylor. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ mpc-audio/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  mpc-audio
+//
+//  Created by Carl  on 05/12/2015.
+//  Copyright © 2015 Carl Taylor. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

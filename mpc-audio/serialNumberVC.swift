9bb64a45faa920d154357a91401e275060e1cf3c
Carl Taylor <carltaylor43uk@gmail.com>
2015-12-11 11:42:41 +0000

    ADDED: progress spinner for network task

--- mpc-audio/serialNumberVC.swift
+++ mpc-audio/serialNumberVC.swift
@@ -11,11 +11,13 @@ import Cocoa
 class serialNumberVC: NSViewController {
     
     @IBOutlet weak var serialNumberTextField: NSTextField!
+    @IBOutlet weak var authoriseButton: NSButton!
+    @IBOutlet weak var progress: NSProgressIndicator!
     
     @IBAction func authoriseButtonPressed(sender: AnyObject) {
-        var serialNumber = serialNumberTextField.stringValue
+        let serialNumber = serialNumberTextField.stringValue
         if (serialNumber.characters.count > 0) {
-//            serialNumber = "965D1A94-33564923-8BD47293-4093BFEF"
+            //            serialNumber = "965D1A94-33564923-8BD47293-4093BFEF"
             AttemptAuthorisationForSerial(serialNumber)
         } else {
             showMessage("Please enter your serial number")
@@ -23,16 +25,27 @@ class serialNumberVC: NSViewController {
     }
     
     func AttemptAuthorisationForSerial(serial: String) {
-            SerialNumberController.attemptAuthorisationForKey(serial) { (userMessage, authSuccess) -> Void in
-                if (authSuccess) {
-                    self.showMessage(userMessage)
-                    self.dismissViewController(self)
-                } else {
-                    self.showMessage(userMessage)
-                }
+        
+        authoriseButton.enabled = false
+        progress.hidden = false
+        progress.startAnimation(self)
+        weak var weakSelf = self
+        
+        SerialNumberController.attemptAuthorisationForKey(serial) { (userMessage, authSuccess) -> Void in
+            weakSelf?.progress.hidden = true
+            weakSelf?.progress.stopAnimation(weakSelf)
+            
+            if (authSuccess) {
+                weakSelf!.showMessage(userMessage)
+                weakSelf!.dismissViewController(self)
+            } else {
+                weakSelf!.showMessage(userMessage)
+                weakSelf!.authoriseButton.enabled = true
+            }
         }
     }
     
+    // MARK: Alert
     func showMessage(message: String) {
         let alert = NSAlert()
         alert.alertStyle = NSAlertStyle.InformationalAlertStyle
